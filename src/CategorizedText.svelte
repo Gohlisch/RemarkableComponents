ff96cfebd3d6196a13f8c22f88633919ad283c25
timgohlisch <tim.gohlisch001@stud.fh-dortmund.de>
2020-10-26 20:24:13 +0100

    fix(CategorizedText): update colors, after the store emitted

--- src/CategorizedText.svelte
+++ src/CategorizedText.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
     import { categoryStore } from "./CategoryStore";
-    import type { Category } from "./Category";
-    import {onMount} from "svelte";
+    import { onMount } from "svelte";
 
     export let text: string = "";
-    let category: Category;
-    let tokenized: string[];
-    $: tokenized = text.split(/([^a-zA-ZäöüÖÄÜ])/i);
-
-    onMount(()=>{categoryStore.subscribe(() => tokenized = tokenized); console.log("pop");},);
+    let tokenized: string[] = [];
+    onMount(()=> {
+	    categoryStore.subscribe(() => tokenized = text.split(/([^a-zA-ZäöüÖÄÜ])/i));
+    });
 </script>
 
 {#each tokenized as token}
-    {#if (category = categoryStore.getByValue(token))}
-        <mark style="background-color: {category.color}">{token}</mark>
+    {#if categoryStore.getByValue(token)}
+        <mark style="background-color: {categoryStore.getByValue(token).color}">{token}</mark>
     {:else}
         <span>{token}</span>
     {/if}
